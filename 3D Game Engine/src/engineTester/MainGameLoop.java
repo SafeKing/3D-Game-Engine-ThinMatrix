43b8c766cecfbd9bde0561bbc720011160c57881
Ritsu <ritsu.chan.95@gmail.com>
2015-02-12 05:55:37 +0100

    #4 & #5
    Coloring using Shaders

--- 3D Game Engine/src/engineTester/MainGameLoop.java
+++ 3D Game Engine/src/engineTester/MainGameLoop.java
@@ -6,15 +6,16 @@ import renderEngine.DisplayManager;
 import renderEngine.Loader;
 import renderEngine.RawModel;
 import renderEngine.Renderer;
+import shaders.StaticShader;
 
 public class MainGameLoop {
 
 	public static void main(String[] args) {
 
 		DisplayManager.createDisplay();
-
 		Loader loader = new Loader();
 		Renderer renderer = new Renderer();
+		StaticShader shader=new StaticShader();
 
 		// OpenGL expects vertices to be defined counter clockwise by default
 		float[] vertices = {
@@ -33,10 +34,13 @@ public class MainGameLoop {
 
 		while (!Display.isCloseRequested()) {
 			renderer.prepare();
+			shader.start();
 			renderer.render(model);
+			shader.stop();
 			DisplayManager.updateDisplay();
 		}
 
+		shader.cleanUp();
 		loader.cleanUp();
 		DisplayManager.closeDisplay();
 
