7e415ca5dbe3eb67abd3d2d5647bed34df0633ea
Ritsu <ritsu.chan.95@gmail.com>
2015-02-12 04:43:48 +0100

    #2
    VAOs and VBOs

--- 3D Game Engine/src/engineTester/MainGameLoop.java
+++ 3D Game Engine/src/engineTester/MainGameLoop.java
@@ -3,6 +3,9 @@ package engineTester;
 import org.lwjgl.opengl.Display;
 
 import renderEngine.DisplayManager;
+import renderEngine.Loader;
+import renderEngine.RawModel;
+import renderEngine.Renderer;
 
 public class MainGameLoop {
 
@@ -10,10 +13,25 @@ public class MainGameLoop {
 
 		DisplayManager.createDisplay();
 
+		Loader loader = new Loader();
+		Renderer renderer = new Renderer();
+
+		// OpenGL expects vertices to be defined counter clockwise by default
+		float[] vertices = {
+				// Left bottom triangle
+				-0.5f, 0.5f, 0f, -0.5f, -0.5f, 0f, 0.5f, -0.5f, 0f,
+				// Right top triangle
+				0.5f, -0.5f, 0f, 0.5f, 0.5f, 0f, -0.5f, 0.5f, 0f };
+
+		RawModel model = loader.loadToVAO(vertices);
+
 		while (!Display.isCloseRequested()) {
+			renderer.prepare();
+			renderer.render(model);
 			DisplayManager.updateDisplay();
 		}
-		
+
+		loader.cleanUp();
 		DisplayManager.closeDisplay();
 
 	}
---
+++ 3D Game Engine/src/renderEngine/RawModel.java
@@ -0,0 +1,21 @@
+package renderEngine;
+
+public class RawModel {
+
+	private int vaoID;
+	private int vertexCount;
+
+	public RawModel(int vaoID, int vertexCount) {
+		this.vaoID = vaoID;
+		this.vertexCount = vertexCount;
+	}
+
+	public int getVaoID() {
+		return vaoID;
+	}
+
+	public int getVertexCount() {
+		return vertexCount;
+	}
+
+}
