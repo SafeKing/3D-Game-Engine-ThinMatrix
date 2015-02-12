45e7492818ce92d352e1b8f319b6b8d7da24d74d
Ritsu <ritsu.chan.95@gmail.com>
2015-02-12 08:15:43 +0100

    #9 & #10
    OBJ File format & Loading 3D Models

--- 3D Game Engine/src/engineTester/MainGameLoop.java
+++ 3D Game Engine/src/engineTester/MainGameLoop.java
@@ -8,6 +8,7 @@ import org.lwjgl.util.vector.Vector3f;
 
 import renderEngine.DisplayManager;
 import renderEngine.Loader;
+import renderEngine.OMJLoader;
 import renderEngine.Renderer;
 import shaders.StaticShader;
 import textures.ModelTexture;
@@ -23,42 +24,16 @@ public class MainGameLoop {
 		StaticShader shader = new StaticShader();
 		Renderer renderer = new Renderer(shader);
 
-		float[] vertices = {
-				-0.5f, 0.5f, -0.5f, -0.5f, -0.5f, -0.5f, 0.5f, -0.5f, -0.5f, 0.5f, 0.5f, -0.5f,
+		RawModel model = OMJLoader.loadObjModel("stall", loader);
 
-				-0.5f, 0.5f, 0.5f, -0.5f, -0.5f, 0.5f, 0.5f, -0.5f, 0.5f, 0.5f, 0.5f, 0.5f,
+		TexturedModel staticModel = new TexturedModel(model, new ModelTexture(loader.loadTexture("stallTexture")));
 
-				0.5f, 0.5f, -0.5f, 0.5f, -0.5f, -0.5f, 0.5f, -0.5f, 0.5f, 0.5f, 0.5f, 0.5f,
-
-				-0.5f, 0.5f, -0.5f, -0.5f, -0.5f, -0.5f, -0.5f, -0.5f, 0.5f, -0.5f, 0.5f, 0.5f,
-
-				-0.5f, 0.5f, 0.5f, -0.5f, 0.5f, -0.5f, 0.5f, 0.5f, -0.5f, 0.5f, 0.5f, 0.5f,
-
-				-0.5f, -0.5f, 0.5f, -0.5f, -0.5f, -0.5f, 0.5f, -0.5f, -0.5f, 0.5f, -0.5f, 0.5f
-
-		};
-
-		float[] textureCoords = {
-
-				0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0
-
-		};
-
-		int[] indices = {
-				0, 1, 3, 3, 1, 2, 4, 5, 7, 7, 5, 6, 8, 9, 11, 11, 9, 10, 12, 13, 15, 15, 13, 14, 16, 17, 19, 19, 17, 18, 20, 21, 23, 23, 21, 22
-
-		};
-
-		RawModel model = loader.loadToVAO(vertices, textureCoords, indices);
-
-		TexturedModel staticModel = new TexturedModel(model, new ModelTexture(loader.loadTexture("image")));
-
-		Entity entity = new Entity(staticModel, new Vector3f(0, 0, -5), 0, 0, 0, 1);
+		Entity entity = new Entity(staticModel, new Vector3f(0, 0, -50), 0, 0, 0, 1);
 
 		Camera camera = new Camera();
 
 		while (!Display.isCloseRequested()) {
-			entity.increaseRotation(1, 1, 0);
+			entity.increaseRotation(0, 1, 0);
 			camera.move();
 			renderer.prepare();
 			shader.start();
--- 3D Game Engine/src/entities/Camera.java
+++ 3D Game Engine/src/entities/Camera.java
@@ -16,13 +16,16 @@ public class Camera {
 
 	public void move() {
 		if (Keyboard.isKeyDown(Keyboard.KEY_W)) {
-			position.z -= 0.02f;
+			position.z -= 0.2f;
+		}
+		if (Keyboard.isKeyDown(Keyboard.KEY_S)) {
+			position.z += 0.2f;
 		}
 		if (Keyboard.isKeyDown(Keyboard.KEY_D)) {
-			position.x += 0.02f;
+			position.x += 0.2f;
 		}
 		if (Keyboard.isKeyDown(Keyboard.KEY_A)) {
-			position.x -= 0.02f;
+			position.x -= 0.2f;
 		}
 	}
 
